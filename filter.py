f60313fba6f28ebbd6b733e1cdb0f34ef6615a54
redboo <garda1@mail.ru>
2023-10-04 04:34:23 +0500

    Использовать стандартное логирование

--- filter.py
+++ filter.py
@@ -1,5 +1,11 @@
+import logging
+
 import requests
-from loguru import logger
+
+# Настройка логгирования
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s | %(levelname)s | %(message)s", datefmt="%Y-%m-%d %H:%M:%S"
+)
 
 
 def get_autobidmaster_data():
@@ -17,17 +23,15 @@ def get_autobidmaster_data():
     if response.status_code == 200:
         return response.json()
     else:
-        logger.error(f"Ошибка запроса. Статус код: {response.status_code}")
+        logging.error(f"Ошибка запроса. Статус код: {response.status_code}")
         return None
 
 
 if __name__ == "__main__":
-    log_format = "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
-    logger.add(sink=lambda message: print(message), level="DEBUG", format=log_format, colorize=True, enqueue=True)
-
     autobidmaster_data = get_autobidmaster_data()
 
     if autobidmaster_data:
-        logger.info("Данные успешно получены: %s", autobidmaster_data)
+        # logging.info("Данные успешно получены: %s", autobidmaster_data)
+        logging.info("Данные успешно получены: ")
     else:
-        logger.error("Не удалось получить данные с сайта.")
+        logging.error("Не удалось получить данные с сайта.")
